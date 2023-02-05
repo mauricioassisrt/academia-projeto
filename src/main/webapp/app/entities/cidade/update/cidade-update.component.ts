0b9121a1be8e78bef5a8fbc816d3232edb8d3d63
Mauricio de Assis <mauriciox8mm@hotmail.com>
2023-02-05 16:07:20 -0300

    finalizando o Campo de pesquisa

--- src/main/webapp/app/entities/cidade/update/cidade-update.component.ts
+++ src/main/webapp/app/entities/cidade/update/cidade-update.component.ts
@@ -23,23 +23,6 @@ export class CidadeUpdateComponent implements OnInit {
   isSaving = false;
   estadosSharedCollection: IEstado[] = [];
   usersSharedCollection: IUser[] = [];
-  nomeEstadoPesquisa: string = "";
-  predicate!: string;
-  ascending!: boolean;
-
-  editFormEstado = this.fb.group({
-    id: [],
-    nomeEstado: [null, [Validators.required, Validators.minLength(3)]],
-    sigla: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
-  });
-
-  editForm = this.fb.group({
-    id: [],
-    nomeCidade: [null, [Validators.required, Validators.minLength(3)]],
-    observacao: [null, [Validators.minLength(20)]],
-    estado: [],
-    user: [],
-  });
 
   constructor(
     protected cidadeService: CidadeService,
@@ -53,19 +36,17 @@ export class CidadeUpdateComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({cidade}) => {
       this.updateForm(cidade);
+      this.estado=cidade.estado
       this.loadRelationshipsOptions();
     });
   }
-
-  selecionarItem(estado: IEstado) {
-    this.editFormEstado.patchValue({
-      id: estado.id,
-      nomeEstado: estado.nomeEstado,
-      sigla: estado.sigla,
-    });
-
-  }
-
+  editForm = this.fb.group({
+    id: [],
+    nomeCidade: [null, [Validators.required, Validators.minLength(3)]],
+    observacao: [null, [Validators.minLength(20)]],
+    estado: [],
+    user: [],
+  });
   previousState(): void {
     window.history.back();
   }
@@ -139,60 +120,46 @@ export class CidadeUpdateComponent implements OnInit {
   }
 
   protected createFromForm(): ICidade {
+
     return {
       ...new Cidade(),
       id: this.editForm.get(['id'])!.value,
       nomeCidade: this.editForm.get(['nomeCidade'])!.value,
       observacao: this.editForm.get(['observacao'])!.value,
-      estado: this.editFormEstado.value,
+      estado: this.estado,
       user: this.editForm.get(['user'])!.value,
     };
   }
 
-  public digitar(valor: any) {
+  /**
+   * Recebe do campo de pesquisa um valor e faz a busca do mesmo no endpoint
+   * no qual retorna paginado o valor que contenha os dados informados
+   * @param objetoEvent
+   */
+  public pesquisarEstadoPeloNome(objetoEvent: any) {
     this.estadosSharedCollection = []
     this.estadoService
       .query({
         page: 0,
         size: 30,
-        'nomeEstado.contains': valor.target.value
+        'nomeEstado.contains': objetoEvent.target.value
       })
       .pipe(map((res: HttpResponse<IEstado[]>) => res.body ?? []))
       .pipe(map((estados: IEstado[]) => this.estadoService.addEstadoToCollectionIfMissing(estados, this.editForm.get('estado')!.value)))
       .subscribe((estados: IEstado[]) => (this.estadosSharedCollection = estados));
 
     if(this.estadosSharedCollection.length==0){
-      this.estadosSharedCollection=[]
-      this.nomeEstado=""
-      this.createFromFormAlte()
-      // this.editFormEstado = this.fb.group({
-      //   id: this.fb.array([]),
-      //   nomeEstado: this.fb.array([]),
-      //
-      // })
      const estado= this.editForm.get('estado')
-      estado?.reset();
-      // this.editForm = this.fb.group({
-      //   estado: this.fb.array([])
-      // })
+     estado?.reset();
      }
   }
 
-  protected sort(): string[] {
-    const result = [this.predicate + ',' + (this.ascending ? ASC : DESC)];
-    if (this.predicate !== 'id') {
-      result.push('id');
-    }
-    return result;
+  /**
+   * Usado para pegar o valor selecionado pelo usuáiro e add no objeto estado
+   * @param estado
+   */
+  selecionarItem(estado: IEstado) {
+    this.estado=estado
   }
-  protected createFromFormAlte(): ICidade {
-    return {
-      ...new Cidade(),
-      id: this.editForm.get(['id'])!.value,
-      nomeCidade: this.editForm.get(['nomeCidade'])!.value,
-      observacao: this.editForm.get(['observacao'])!.value,
-      estado: {},
-      user: this.editForm.get(['user'])!.value,
-    };
-  }
+
 }
